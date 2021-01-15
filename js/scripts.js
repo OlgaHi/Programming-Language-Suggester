0a3bfae18da4fa0e9921136f155dc143622539f6
Olha Hizhytska <olgainfotech@gmail.com>
2021-01-15 12:28:32 -0800

    Add basic logic of branching to choose the path

--- js/scripts.js
+++ js/scripts.js
@@ -7,10 +7,21 @@ $(document).ready(function() {
     const answer2 = $("input:radio[name=question2]:checked").val();
     const answer3 = $("input:radio[name=question3]:checked").val();
     const answer4 = $("input:radio[name=question4]:checked").val();
-    
+
     $(".user_name").text(name);
-    
-    
+    if (age > 10 && answer2 === "yes") {
+      $("#suggestion1").show();
+    } else if (answer1 === "yes" || answer2 === "yes" ){
+      $("#suggestion2").show();
+    } else {
+      $("#suggestion3").show();
+    }
+
+    $(".user_name").text(name);
+    $(this)[0].reset(); 
+
     
   }); 
 });
+
+
