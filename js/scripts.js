8d2c01365b81ab0b7a719cd33cdce384049421bd
Olha Hizhytska <olgainfotech@gmail.com>
2021-01-15 10:59:21 -0800

    Add logic to collect the date from first 3 question

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
-  
-  
+  $("form#suggestion-form").submit(function(event) {
+    event.preventDefault();
+    const name = $("#name").val();
+    const age = parseInt($("#age").val());
+    const answer1 = $("input:radio[name=option]:checked").val();
+    console.log(answer1)
+  }); 
 });
